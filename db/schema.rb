92ad0ecca989d273979d80c7b7c44544226158c6
fran <francisco.chiarino@gmail.com>
2020-10-26 14:37:29 +0100

    Migrated data using "rake db:migrate"

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_26_091124) do
+ActiveRecord::Schema.define(version: 2020_10_26_133451) do
 
   create_table "items", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "completed_at"
   end
 
 end
